576797ae6eea66a8f931a7dcadfe6b18b68aca2c
pawanchichirya <pawanchichriya3@gmail.com>
2023-02-11 21:48:40 +0530

    completed persistance layer and PlayerDaoFactory

--- PracticeCRUD/src/com/test/daofactory/PlayerDaoFactory.java
+++ PracticeCRUD/src/com/test/daofactory/PlayerDaoFactory.java
@@ -1,6 +1,18 @@
 package com.test.daofactory;
 
+import com.test.persistance.IPlayerDao;
+import com.test.persistance.PlayerDaoImpl;
+
 public class PlayerDaoFactory {
 
+	private PlayerDaoFactory() {};
+	
+	private static IPlayerDao playerDao=null;
+	
+	public static IPlayerDao getPlayerDao() {
+		if(playerDao==null)
+			playerDao=new PlayerDaoImpl();
+		return playerDao;
+	}
 	
 }
